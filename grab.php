5175051d704059f91f4c7eaa470df7d5a93bd55e
Freyze <mcfreyze@gmail.com>
2020-09-13 23:32:08 +0300

    init

--- grab.php
+++ grab.php
@@ -1,14 +1,23 @@
 <?php
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
 include('simple_html_dom.php');
 $searchQuery = $_GET['query'];
 
 $searchQuery = str_replace(' ','+',$searchQuery);
 $curl = curl_init();
+
 curl_setopt($curl, CURLOPT_URL, "https://www.google.com/search?q=" . $searchQuery);
 curl_setopt($curl, CURLOPT_FOLLOWLOCATION,true);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36');
+
 
 $result = curl_exec($curl);
+var_dump($result);
 curl_close($curl);
 
 $domResults = new simple_html_dom();
@@ -16,5 +25,6 @@ $domResults = new simple_html_dom();
 $domResults->load($result);
 
 foreach ($domResults->find('div.r') as $elements){
-    echo $elements . '</br>';
+    echo $elements->find('h3', 0)->plaintext . '</br>';
+    echo $elements->find('a', 0)->href . '</br>';
 }
